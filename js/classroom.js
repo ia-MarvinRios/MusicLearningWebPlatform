12d530eeb931e4277fcb680829fe9a62143023b3
Marvin Rios <ia.marvinrios@ufg.edu.sv>
2024-05-18 20:33:36 -0600

    Beta 0.6.2

--- js/classroom.js
+++ js/classroom.js
@@ -12,19 +12,6 @@ document.addEventListener("DOMContentLoaded", function() {
     if (mediaQuery.matches) {
         console.log('HAMBURGUESO NORMAL.');
 
-        menu.classList.toggle("disabled")
-        const openButton = document.querySelector('.nav__menu');
-        const menu = document.querySelector('.nav__link');
-        const closeMenu = document.querySelector('.nav__close');
-
-        openButton.addEventListener('click', ()=>{
-            menu.classList.add("nav__link--show");
-        })
-
-        closeMenu.addEventListener('click', ()=>{
-            menu.classList.remove("nav__link--show");
-        })
-
     } else {
         console.log('HAMBURGUESO DESPLEGABLE.');
 
